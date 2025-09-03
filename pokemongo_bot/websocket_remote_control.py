88f6c0a4d522d108b570b57981982c3358d20a8d
Caleb Harden <cbh32890@gmail.com>
2025-09-02 19:19:42 -0500

    fixed socketio in websocket remote control file

--- pokemongo_bot/websocket_remote_control.py
+++ pokemongo_bot/websocket_remote_control.py
@@ -1,36 +1,26 @@
+# pokemongo_bot/websocket_remote_control.py
 import threading
 import logging
-from socketIO_client import BaseNamespace
-from socketIO_client import SocketIO
+import socketio
 from pokemongo_bot import inventory
 
-class WebsocketRemoteControl(object):
-
-
-    def __init__(self, bot):
+class WebsocketNamespace(socketio.ClientNamespace):
+    def __init__(self, bot, namespace):
+        super().__init__(namespace)
         self.bot = bot
-        self.host, port_str = self.bot.config.websocket_server_url.split(':')
-        self.port = int(port_str)
-        self.sio = SocketIO(self.host, self.port)
-        self.sio.on(
-            'bot:process_request:{}'.format(self.bot.config.username),
-            self.on_remote_command
-        )
-        self.thread = threading.Thread(target=self.process_messages)
-        self.logger = logging.getLogger(type(self).__name__)
+        self.logger = logging.getLogger(__name__)
 
-    def start(self):
-        self.thread.start()
-        return self
+    def on_connect(self):
+        self.logger.info(f"Connected to websocket server for {self.bot.config.username}")
 
-    def process_messages(self):
-        self.sio.wait()
+    def on_disconnect(self):
+        self.logger.info(f"Disconnected from websocket server for {self.bot.config.username}")
 
-    def on_remote_command(self, command):
+    def on_bot_process_request(self, command):
         name = command['name']
         command_handler = getattr(self, name, None)
         if not command_handler or not callable(command_handler):
-            self.sio.emit(
+            self.emit(
                 'bot:send_reply',
                 {
                     'response': '',
@@ -40,13 +30,13 @@ class WebsocketRemoteControl(object):
             )
             return
         if 'args' in command:
-            command_handler(*args)
-            return
-        command_handler()
+            command_handler(*command['args'])
+        else:
+            command_handler()
 
     def get_player_info(self):
         try:
-            self.sio.emit(
+            self.emit(
                 'bot:send_reply',
                 {
                     'result': {'inventory': inventory.jsonify_inventory(), 'player': self.bot._player},
@@ -55,4 +45,27 @@ class WebsocketRemoteControl(object):
                 }
             )
         except Exception as e:
-            self.logger.error(e)
+            self.logger.error(f"Error in get_player_info: {e}")
+
+class WebsocketRemoteControl(object):
+    def __init__(self, bot):
+        self.bot = bot
+        self.host, port_str = self.bot.config.websocket_server_url.split(':')
+        self.port = int(port_str)
+        self.sio = socketio.Client()
+        self.namespace = WebsocketNamespace(self.bot, f"/{self.bot.config.username}")
+        self.sio.register_namespace(self.namespace)
+        self.thread = threading.Thread(target=self.process_messages)
+        self.logger = logging.getLogger(__name__)
+
+    def start(self):
+        self.sio.connect(f"ws://{self.host}:{self.port}")
+        self.thread.start()
+        return self
+
+    def process_messages(self):
+        self.sio.wait()
+
+    def on_remote_command(self, command):
+        # This method is no longer needed as it's handled by WebsocketNamespace
+        pass
